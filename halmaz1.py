50b64a70760f5341cba8556ef423831b36f37805
ltamas07 <93704068+ltamas07@users.noreply.github.com>
2022-12-06 12:02:03 +0100

    Python File

---
+++ halmaz1.py
@@ -0,0 +1,24 @@
+# reggeli = {'Víz', 'Tea', 'Vaj', 'Pirítós'}
+# ebed = {'Víz', 'Halászlé', 'Kenyér'}
+
+# print(reggeli & ebed)
+# print(reggeli | ebed)
+# print(reggeli - ebed)
+# print(reggeli ^ ebed)
+
+# gyumolcskosar = ['Eper', 'Alma', 'Szilva', 'Barack', 'Körte']
+# fajta = set()
+# for gyumolcs in gyumolcskosar:
+#     fajta.add(gyumolcs)
+# print(fajta)
+
+
+## FELADATOK @@
+
+
+vacsora = {'Krumplistarhonya', 'Húsleves', 'Paradicsom szósz', 'Aranygaluska'}
+print(vacsora)
+vacsora.remove('Húsleves')
+print(vacsora)
+vacsora.clear()
+print(vacsora)
